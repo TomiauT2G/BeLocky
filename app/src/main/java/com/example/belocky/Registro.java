57ab796ab971e6db4b421a7569e0d7424235346b
TomiauT2G <t.bravo01@ufromail.cl>
2023-11-27 20:31:35 -0300

    login y Registro creado falta instalacion y creacion de firestore de firebase

---
+++ app/src/main/java/com/example/belocky/Registro.java
@@ -0,0 +1,77 @@
+package com.example.belocky;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class Registro extends AppCompatActivity {
+
+    Button btn_register;
+    EditText name, email, password;
+    FirebaseFirestore mFirestore;
+    FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro);
+        this.setTitle("Registro");
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+
+        mFirestore = FirebaseFirestore.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+
+        name = findViewById(R.id.nombre);
+        email = findViewById(R.id.correo);
+        password = findViewById(R.id.contrasena);
+        btn_register = findViewById(R.id.btn_registro);
+
+        btn_register.setOnClickListener(view -> {
+            String nameUser = name.getText().toString().trim();
+            String emailUser = email.getText().toString().trim();
+            String passUser = password.getText().toString().trim();
+
+            if (nameUser.isEmpty() && emailUser.isEmpty() && passUser.isEmpty()){
+                Toast.makeText(Registro.this, "Complete los datos", Toast.LENGTH_SHORT).show();
+            }else{
+                registerUser(nameUser, emailUser, passUser);
+            }
+        });
+    }
+
+    private void registerUser(String nameUser, String emailUser, String passUser) {
+        mAuth.createUserWithEmailAndPassword(emailUser, passUser).addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                FirebaseUser user = mAuth.getCurrentUser();
+                if (user != null) {
+                    String id = user.getUid();
+                    Map<String, Object> map = new HashMap<>();
+                    map.put("id", id);
+                    map.put("name", nameUser);
+                    map.put("email", emailUser);
+                    map.put("password", passUser);
+
+                    mFirestore.collection("user").document(id).set(map).addOnSuccessListener(unused -> {
+                        finish();
+                        startActivity(new Intent(Registro.this, MainActivity.class));
+                        Toast.makeText(Registro.this, "Usuario registrado con éxito", Toast.LENGTH_SHORT).show();
+                    }).addOnFailureListener(e -> Toast.makeText(Registro.this, "Error al guardar", Toast.LENGTH_SHORT).show());
+                }
+            } else {
+                Toast.makeText(Registro.this, "Error al registrar el usuario", Toast.LENGTH_SHORT).show();
+            }
+        });
+}
+}
