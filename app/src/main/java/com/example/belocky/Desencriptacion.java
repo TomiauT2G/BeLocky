9cf6629d10a4c8d7bca989a8115d7ba4cd0c090e
TomiauT2G <t.bravo01@ufromail.cl>
2023-11-27 00:17:37 -0300

    Creado De Encritacion y Desencriptacion

--- app/src/main/java/com/example/belocky/Desencriptacion.java
+++ app/src/main/java/com/example/belocky/Desencriptacion.java
@@ -5,10 +5,10 @@ import javax.crypto.SecretKey;
 import java.util.Base64;
 
 public class Desencriptacion {
-    private static String desencriptar(String mensajeEncriptado, SecretKey clave) throws Exception {
+    private static String desencriptar(String ContraseñaEncritada, SecretKey clave) throws Exception {
         Cipher cipher = Cipher.getInstance("AES");
         cipher.init(Cipher.DECRYPT_MODE, clave);
-        byte[] datosDesencriptados = cipher.doFinal(Base64.getDecoder().decode(mensajeEncriptado));
+        byte[] datosDesencriptados = cipher.doFinal(Base64.getDecoder().decode(ContraseñaEncritada));
         return new String(datosDesencriptados);
     }
 }
