c1bb7eaee045a9c9a1deedf59277496a408ced97
TomiauT2G <t.bravo01@ufromail.cl>
2023-12-01 00:04:17 -0300

    PORFAVOR GITHUB DEJAME PUSHEAR

--- app/src/main/java/com/example/belocky/MainActivity.java
+++ app/src/main/java/com/example/belocky/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.belocky;
 
+import android.annotation.SuppressLint;
+import android.media.MediaPlayer;
+import android.widget.ImageView;
+import android.widget.VideoView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView Logo ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Logo = findViewById(R.id.Logo);
+        Logo.setImageResource(R.drawable.belockywhite);
+        VideoView videoView = (VideoView) findViewById(R.id.videoView);
+        videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.z);
+        videoView.start();
+        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mp.setLooping(true);
+            }
+        });
     }
 }
